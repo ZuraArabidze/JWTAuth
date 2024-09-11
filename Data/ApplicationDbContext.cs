b28d237ee1dc3bbe418a13753166fb211b375564
ZuraArabidze <z.arabidzee@gmail.com>
2024-09-11 13:16:07 +0400

    Edit namespace in ApplicationDbContext Create Pages and Users Controller

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace JwtRoleAuthentication.Data;
+namespace JWTAuth.Data;
 
 
 public class ApplicationDbContext : IdentityUserContext<ApplicationUser>
